9fe591d445c054a7fe873613cdacad7071542dbe
danmartensen <danmartensen@BBCOMs-MacBook-Pro-6.local>
2015-05-21 10:24:51 -0600

    adding Node factory, type checking and API interface

--- k-ary-tree.js
+++ k-ary-tree.js
@@ -1,141 +1,77 @@
-/**
-*   Author: Dan Martensen
-*
-*   Simple example of k-ary tree with insertChild(), insertSibling(),
-*   and removeChild() operations, along with set and remove parent
-*   references if needed.
-*
-*
-*   Each operation does a Breadth First Search thru the tree iteratively
-*   with O(n) time complcompletedy.
-*
-*   Could easily be integrated into the Model layer of a UI, allowing 
-*   click events to call these functions which act on the tree data structure.  
-*   Each node has a data property and array of children nodes.  This could 
-*   be scaled to more properties, based on UI needs such as image paths, urls, 
-*   numbers, and more.
-*/
 (function() {
-
+    'use strict';
     /**
-    *   Root node in tree with data and children array properties.
+    *   Author: Dan Martensen
+    *
+    *   Simple example of k-ary tree with insertChild(), insertSibling(),
+    *   and removeChild() operations, along with set and remove parent
+    *   references if needed.
+    *
+    *   Each operation does a Breadth First Search thru the tree iteratively
+    *   with O(n) time complexity.
+    *
+    *   Could easily be integrated into an Angular Model layer of a UI, enabling 
+    *   click events to call these functions which act on the tree data structure.  
+    *   Each node has a data property and array of children nodes.  This could 
+    *   be scaled to more properties, based on UI needs such as image paths, urls, 
+    *   numbers, and more.
     */
-    var _rootNode = {
-        data: '',
-        children: []
-    };
+    var Tree = (function() {
 
-    /**
-    *   @function addSiblingNode()
-    *
-    *   @description 
-    *   Inserts new node as sibling to target node. 
-    *
-    *   Private function does BFS thru tree structure using a queue.  For each
-    *   node's child checks if target node matches node's child references.
-    *   If so, the node is parent so insert in the node via splice().
-    *
-    *   This ensures the sibling is added immediately following the node (target) 
-    *   that invoked the operation.  
-    *
-    *   @param target - node that invoked the operation (e.g Button click event)
-    *   @returns completed - boolean indicates if operation was successful.
-    */
-    function addSiblingNode(target) {
-        var queue = [], completed = false, node = null, i;
+        /**
+        *   Single node is created thru a factory function instead instance 
+        *   object so it's less vulnerable to instance (this) manipulations.
+        *
+        *   @param data - data string value to be stored in node
+        *   @param parent - reference to parent node
+        *   @param children - array of child nodes
+        */
+        function Node(data, parent, children) {
 
-        queue.push(_rootNode);
+            // type check parameters
+            if(Object.prototype.toString.call(data) === '[object String]' &&
+                Array.isArray(children)) {
 
-        //  Breadth first search with early exit
-        while(queue.length !== 0 && completed === false) {
-            node = queue.shift();
+                return {
+                    data: data,
+                    parent: parent,
+                    children: children
+                };
 
-            for(i = 0; i < node.children.length && completed === false; i++) {
-
-                // node's child matches target, meaning
-                if(node.children[i] === target) {
-
-                    node.children.splice(i + 1, 0,
-                        {
-                            data: '',
-                            parent: node,
-                            children: []
-                        }
-                    );
-
-                    completed = true;
-                } else {
-                    queue.push(node.children[i]);
-                }
-            }
-        }
-
-        return completed;
-    }
-
-    /**
-    *   @function addChildNode()
-    *
-    *   @description 
-    *   Inserts a new node as target node's child.
-    *
-    *   Private function does BFS thru tree structure using a queue.  For each
-    *   node's child checks if target's node matches node's reference.
-    *   If so, the node is target so insert in the node into it's child array 
-    *   via push().  Returns true
-    *
-    *   @param target - node that invoked the operation (e.g Button click event)
-    *   @returns completed - boolean indicates if operation was successful.
-    */
-    function addChildNode(target) {
-        var queue = [],
-            completed = false, node = null, i;
-
-        queue.push(_rootNode);
-
-        //  Breadth first search with early exit
-        while(queue.length !== 0 && completed === false) {
-            node = queue.shift();
-
-            if(node.data === target.data) {
-                node.children.push(
-                    {
-                        data: '',
-                        parent: node,
-                        children: []
-                    }
-                );
-                completed = true;
             } else {
-                for(i = 0 ; i < node.children.length && completed === false; i++) {
-                    queue.push(node.children[i]);
-                }
+                console.log('Invalid data');
             }
         }
 
-        return completed;
-    }
+        /**
+        *   Defines private root node in tree with empty properties. It's the
+        *   one exception from Node nodes because root nodes don't
+        *   have parent references.
+        */
+        var _rootNode = {
+            data: '',
+            parent: null,
+            children: []
+        };
 
-    /**
-    *   @function removeNode()
-    *
-    *   @description 
-    *   Removes target node from tree. 
-    *
-    *   Private function first checks that node isn't last node.
- 
-    *   Then does BFS thru tree structure using a queue.  For each
-    *   node's child checks if target node matches node's child references.
-    *   If so, the node is parent so remove the node via splice().
-    *
-    *   @param target - node that invoked the operation (e.g Button click event)
-    *   @returns completed - boolean indicates if operation was successful.
-    */
-    function removeNode(target) {
-        var queue = [],
-            completed = false, node = null, i;
-
-        if(_rootNode.children.length !== 0) {
+        /**
+        *   @function addSiblingNode()
+        *
+        *   @description 
+        *   Inserts new node as sibling to target node. 
+        *
+        *   Private function does BFS thru tree structure using a queue.  For each
+        *   node's child checks if target node matches node's child references.
+        *   If so, the node is parent so insert in the node via splice().
+        *
+        *   This ensures the sibling is added immediately following the node (target) 
+        *   that invoked the operation.  
+        *
+        *   @param target - node that invoked the operation (e.g Button click event)
+        *   @returns completed - boolean indicates if operation was successful.
+        */
+        function insertSiblingNode(target) {
+            var queue = [], completed = false, node = null, i;
 
             queue.push(_rootNode);
 
@@ -145,66 +81,187 @@
 
                 for(i = 0; i < node.children.length && completed === false; i++) {
 
+                    // node's child matches target, meaning
                     if(node.children[i] === target) {
-                        node.children.splice(i, 1);
+
+                        node.children.splice(i + 1, 0, Node('', node, []));
+
                         completed = true;
                     } else {
                         queue.push(node.children[i]);
                     }
                 }
             }
+
+            return completed;
         }
 
-        return completed;
-    }
+        /**
+        *   @function addChildNode()
+        *
+        *   @description 
+        *   Inserts a new node as target node's child.
+        *
+        *   Private function does BFS thru tree structure using a queue.  For each
+        *   node's child checks if target's node matches node's reference.
+        *   If so, the node is target so insert in the node into it's child array 
+        *   via push().  Returns true
+        *
+        *   @param target - node that invoked the operation (e.g Button click event)
+        *   @returns completed - boolean indicates if operation was successful.
+        */
+        function insertChildNode(target) {
+            var queue = [], completed = false, node = null, i;
 
+            queue.push(_rootNode);
 
-    /**
-    *   Sets parent references in tree data structure after JSON loads.
-    */
-    function setParentReferences() {
-        var queue = [], node = null, i;
+            //  Breadth first search with early exit
+            while(queue.length !== 0 && completed === false) {
+                node = queue.shift();
 
-        queue.push(_rootNode);
-
-        //  Breadth first search with early exit
-        while(queue.length !== 0) {
-            node = queue.shift();
-
-            for(i = 0 ; i < node.children.length; i++) {
-
-                // sets childs reference to parent
-                node.children[i].parent = node;
-
-                queue.push(node.children[i]);
+                if(node === target) {
+                    node.children.push(Node('', node, []));
+                    completed = true;
+                } else {
+                    for(i = 0 ; i < node.children.length && completed === false; i++) {
+                        queue.push(node.children[i]);
+                    }
+                }
             }
 
+            return completed;
         }
-        return true;
-    }
 
-    /**
-    *   Removes parent references in tree structure for JSON encoding.
-    */
-    function removeParentReferences() {
-         var queue = [], node = null, i;
+        /**
+        *   @function removeNode()
+        *
+        *   @description 
+        *   Removes target node from tree. 
+        *
+        *   Private function first checks that node isn't last node.
+     
+        *   Then does BFS thru tree structure using a queue.  For each
+        *   node's child checks if target node matches node's child references.
+        *   If so, the node is parent so remove the node via splice().
+        *
+        *   @param target - node that invoked the operation (e.g Button click event)
+        *   @returns completed - boolean indicates if operation was successful.
+        */
+        function removeNode(target) {
+            var queue = [],
+                completed = false, node = null, i;
 
-        queue.push(_rootNode);
+            if(_rootNode.children.length !== 0) {
 
-        //  Breadth first search with early exit
-        while(queue.length !== 0) {
-            node = queue.shift();
+                queue.push(_rootNode);
 
-            for(i = 0 ;i < node.children.length; i++) {
+                //  Breadth first search with early exit
+                while(queue.length !== 0 && completed === false) {
+                    node = queue.shift();
 
-                // removes childs reference to parent
-                delete node.children[i].parent;
+                    for(i = 0; i < node.children.length && completed === false; i++) {
 
-                queue.push(node.children[i]);
+                        if(node.children[i] === target) {
+                            node.children.splice(i, 1);
+                            completed = true;
+                        } else {
+                            queue.push(node.children[i]);
+                        }
+                    }
+                }
             }
-        }
-        return true;
-    }
 
+            return completed;
+        }
+
+
+        /**
+        *   Sets parent references in tree data structure after JSON loads.
+        *
+        *   @param root - root of tree
+        */
+        function setParentReferences(root) {
+            var queue = [], node = null, i;
+
+            queue.push(root);
+
+            //  Breadth first search with early exit
+            while(queue.length !== 0) {
+                node = queue.shift();
+
+                for(i = 0 ; i < node.children.length; i++) {
+
+                    // sets childs reference to parent
+                    node.children[i].parent = node;
+
+                    queue.push(node.children[i]);
+                }
+
+            }
+            return true;
+        }
+
+        /**
+        *   Removes parent references in tree structure for JSON encoding.
+        *
+        *   @param root - root of tree
+        */
+        function removeParentReferences(root) {
+            var queue = [], node = null, i;
+
+            queue.push(root);
+
+            //  Breadth first search with early exit
+            while(queue.length !== 0) {
+                node = queue.shift();
+
+                for(i = 0 ;i < node.children.length; i++) {
+
+                    // removes childs reference to parent
+                    delete node.children[i].parent;
+
+                    queue.push(node.children[i]);
+                }
+            }
+            return true;
+        }
+
+        /**
+        *   JavaScript API providing get and set functions via
+        *   object literal.
+        *
+        *   This could easily be replaced with event handlers
+        *   to elements that represent nodes.
+        */
+        return {
+            getRoot: function() {
+                return _rootNode;
+            },
+            insertChild: function(node) {
+                return insertChildNode(node);
+            },
+            insertSibling: function(node) {
+                return insertSiblingNode(node);
+            },
+            remove: function(node) {
+                return removeNode(node);
+            }
+        };
+
+    })();
+
+    // TEST
+    var root = Tree.getRoot();
+
+    Tree.insertChild(root);
+    Tree.insertChild(root);
+    Tree.insertSibling(root.children[0]);
+
+    Tree.insertChild(root.children[0]);
+    Tree.insertChild(root.children[0]);
+
+    Tree.remove(root.children[1]);
+
+    console.log(root);
 
 })();
